5c604b9d70578d4f48a910e0c3e4078d97dd57dc
KAISET <alejandrootiniano10@gmail.com>
2026-05-20 02:05:16 -0500

    Modify test

--- FacturadorSunatTest/XmlBuilderBlTests.cs
+++ FacturadorSunatTest/XmlBuilderBlTests.cs
@@ -9,7 +9,7 @@ namespace FacturadorSunatTest;
 public class XmlBuilderBlTests
 {
     [Fact]
-    public void BuildSignatureXML_ConDatosValidos_DebeRetornarXmlExitoso()
+    public void BuildInvoiceXML_Ok()
     {   
         var datosPruebaFirma = new DigitalSignature
         {
@@ -24,7 +24,29 @@ public class XmlBuilderBlTests
             DigestValue = "vafds89723hfsf="
         };
 
-        OperationResult<string> resultado = XmlBuilderBl.Instance.BuildSignatureXML(datosPruebaFirma);
+        OperationResult<String> resultado = XmlBuilderBl.Instance.BuildXML(datosPruebaFirma);
+
+        Assert.True(resultado.Success);
+        Assert.NotNull(resultado.Data);
+    }
+
+    [Fact]
+    public void BuildSignatureXML_Ok()
+    {   
+        var datosPruebaFirma = new DigitalSignature
+        {
+            Id = "SIGNATURE_KAISET",
+            SignatureValue = "Abc123ValueBase64String...",
+            X509Certificate = "MIIFpDCCA4SgAwIBAgIQ...",
+            CanonicalizationMethodAlgorithm = "http://www.w3.org/2001/10/xml-exc-c14n#",
+            SignatureMethodAlgorithm = "http://www.w3.org/2000/09/xmldsig#rsa-sha1",
+            ReferenceUri = "#DOCUMENTO-ID",
+            TransformAlgorithm = "http://www.w3.org/2000/09/xmldsig#enveloped-signature",
+            DigestMethodAlgorithm = "http://www.w3.org/2000/09/xmldsig#sha1",
+            DigestValue = "vafds89723hfsf="
+        };
+
+        OperationResult<XmlTagsUBLExtensionXMLDSIG> resultado = XmlBuilderBl.Instance.BuildSignatureXML(datosPruebaFirma);
 
         Assert.True(resultado.Success);
         Assert.NotNull(resultado.Data);
